ae7c6f8d52d71e5b61a75ed1eaa210daacee0dbf
ston <ston.jia@qq.com>
2023-05-23 19:14:01 +0800

    add more func in utils

--- src/main.c
+++ src/main.c
@@ -1,1 +1,13 @@
+#include "utils.h"
 #include <stdio.h>
+#include "map.h"
+
+int
+main ()
+{
+  while (1)
+    {
+      show_menu ();
+      enter_menu (prompt ());
+    }
+}
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-void show_menu(void);
+// 打印主菜单
+void show_menu (void);
+
+// 打印输入提示，返回值为用户输入
+int prompt (void);
+
+// 进入用户选择的选单
+void enter_menu (int);
 
 #endif
