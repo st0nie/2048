e1ffa7ef63f370daf14970c7e80173a8e42a199b
star <2022678928@qq.com>
2023-06-07 19:38:53 +0800

    add history and prompt

--- src/map.c
+++ src/map.c
@@ -1,5 +1,6 @@
 #include "map.h"
 #include "rand.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,4 +13,37 @@ GAMEMAP *map_create(void) {
   random_num(my_map);
   random_num(my_map);
   return my_map;
+}
+
+void map_save(GAMEMAP *my_map, int time) {
+  char ti[100] = {0};
+  sprintf(ti, "%d", time);
+  FILE *fp;
+  int i, d;
+  fp = fopen(ti, "w");
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+
+      fprintf(fp, "%d ", my_map->map[i][j]);
+    }
+  }
+  fprintf(fp,"|%d",my_map->score);
+  fclose(fp);
+}
+
+void map_load(char *name, GAMEMAP *my_map) {
+  FILE *fp;
+  fp = fopen(name, "r");
+  if(fp==NULL)
+  {
+    printf("invalid filename!!!\n");
+    return;
+  }
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      fscanf(fp,"%d",&(my_map->map[i][j]));
+    }
+  }
+  fscanf(fp,"|%d",my_map->score);
+  fclose(fp);
 }
